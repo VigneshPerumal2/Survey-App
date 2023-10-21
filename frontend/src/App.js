d3de85541a3dafedf1fa0cb631c4d5d7b471600d
vigneshperumal2498 <perumalsamy.v@northeastern.edu>
2023-10-21 11:44:16 -0400

    Implemented Admin Dashboard and Admin Login Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,120 @@
 // src/App.js
 
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import SurveyComponent from "./components/SurveyComponent";
+import AdminDashboard from "./components/AdminDashboard";
 import { useAuth0 } from "@auth0/auth0-react";
+import {
+  Button,
+  TextField,
+  Container,
+  Typography,
+  CssBaseline,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-  return (
-    <div className="App">
-      {!isAuthenticated && (
-        <button
-          onClick={() => {
-            console.log("Attempting to log in...");
-            loginWithRedirect();
-          }}
-        >
-          Log in
-        </button>
-      )}
+  // Admin state and handlers
+  const [isAdminAuthenticated, setAdminAuthenticated] = useState(false);
+  const [adminUsername, setAdminUsername] = useState("");
+  const [adminPassword, setAdminPassword] = useState("");
+  const [showAdminLoginForm, setShowAdminLoginForm] = useState(false); // New state
 
-      {isAuthenticated && (
-        <>
-          <SurveyComponent />
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log out
-          </button>
-        </>
-      )}
-    </div>
+  const handleAdminLogin = () => {
+    if (adminUsername === "admin" && adminPassword === "admin123") {
+      setAdminAuthenticated(true);
+      setShowAdminLoginForm(false); // Hide form after successful login
+    } else {
+      alert("Invalid Admin Credentials");
+    }
+  };
+
+  const handleAdminLogout = () => {
+    setAdminAuthenticated(false);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Survey Portal
+        </Typography>
+        <div className="App">
+          {isAdminAuthenticated ? (
+            <>
+              <AdminDashboard />
+              <Button onClick={handleAdminLogout}>Admin Logout</Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={() => setShowAdminLoginForm(true)}>
+                Admin Login
+              </Button>
+              {showAdminLoginForm && (
+                <div>
+                  <TextField
+                    type="text"
+                    placeholder="Admin Username"
+                    value={adminUsername}
+                    onChange={(e) => setAdminUsername(e.target.value)}
+                  />
+                  <TextField
+                    type="password"
+                    placeholder="Admin Password"
+                    value={adminPassword}
+                    onChange={(e) => setAdminPassword(e.target.value)}
+                  />
+                  <Button onClick={handleAdminLogin}>Submit</Button>
+                </div>
+              )}
+            </>
+          )}
+          <hr /> {/* Separator for clarity */}
+          {!isAuthenticated && (
+            <Button
+              onClick={() => {
+                console.log("Attempting to log in...");
+                loginWithRedirect();
+              }}
+            >
+              User Login
+            </Button>
+          )}
+          {isAuthenticated && (
+            <>
+              <SurveyComponent />
+              <Button
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                User Logout
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+    </Container>
   );
 }
 
---
+++ frontend/src/components/AdminDashboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+// Add imports for chart libraries if needed
+
+const AdminDashboard = () => {
+  // Fetch the survey data and generate charts
+
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      {/* Display your charts here */}
+      <div>Chart 1 ...</div>
+      <div>Chart 2 ...</div>
+      {/* ... */}
+    </div>
+  );
+};
+
+export default AdminDashboard;
