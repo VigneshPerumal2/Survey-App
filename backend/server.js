d24a543b3ee16771d4477520e24fed918c41c77e
vigneshperumal2498 <perumalsamy.v@northeastern.edu>
2023-10-21 19:50:28 -0400

    Backend Problem Resolved

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,13 @@ const surveyRoutes = require("./routes/survey");
 const app = express();
 
 // Connect to MongoDB (assuming local MongoDB instance)
-mongoose.connect("mongodb://localhost:27017/fitnessSurvey", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://vigneshp2498:2FY6ahngVyZ2lfr2@cluster0.ylk5rcn.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Middlewares
 app.use(bodyParser.json());
